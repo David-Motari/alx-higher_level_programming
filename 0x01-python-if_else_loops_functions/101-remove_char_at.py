5525d2b9b73afbe06fc41021bd3be34b8e37cbec
David Motari <davemotari22@gmail.com>
2022-06-02 10:25:00 +0000

    Task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def remove_char_at(str, n):
-"""
-Removes the character at position n is string
-arg
-str: string
-n: integer
-Return:
-a copy of a string without chacter n
-"""
-return str[:] if n < 0 else str[:n] + str[n + 1:]
+    """
+    Removes the character at position n is string
+        arg:
+    str: string
+    n: integer
+    Return:
+    a copy of a string without chacter n
+    """
+    return str[:] if n < 0 else str[:n] + str[n + 1:]
