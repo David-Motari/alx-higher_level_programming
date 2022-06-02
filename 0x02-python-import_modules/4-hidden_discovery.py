ff620e473bd3bcff8744922c0e629c280e7e97b4
David Motari <davemotari22@gmail.com>
2022-06-02 15:50:03 +0000

    Task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import inspect
+import hidden_4
+
+if __name__ == "__main__":
+
+    for name in sortedd(dir(hidden_4)):
+
+        if name[:2] != "__":
+
+            print(name)
