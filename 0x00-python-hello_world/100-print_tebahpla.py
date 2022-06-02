e643e3bf9e792c7d8c5827662180512338a5c6b3
under-ground-dog <58317281+under-ground-dog@users.noreply.github.com>
2022-06-02 14:20:56 +0300

    Delete 100-print_tebahpla.py

--- 0x00-python-hello_world/100-print_tebahpla.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-up_alph = False
-for i in range(25, -1, -1):
-    xter = i + ord('A') if up_alph else i + ord('a')
-    print("{:c}".format(xter), end="")
-    up_alph = not up_alph
