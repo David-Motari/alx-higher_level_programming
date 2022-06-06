8a01f08562e6a78383c57ca37cbf3f864a42c4e7
David Motari <davemotari22@gmail.com>
2022-06-06 15:39:40 +0000

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,5 @@
 
 
 def no_c(my_string):
-    another_string = my_string.translate({ord('C'):None})
-    another_string = my_string.translate({ord('c'):None})
+    another_string = my_string.translate({ord(i): None for i in "Cc"})
     return another_string
