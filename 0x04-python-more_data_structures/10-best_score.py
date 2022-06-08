6d1810895a8fba83d0cb64a1df0bd06fb665fdef
dave motari <davemotari22@gmail.com>
2022-06-09 01:08:52 +0300

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+# Function that returns best score or None if dictionary is empty.
+def best_score(a_dictionary):
+    if a_dictionary:
+        b_key = max(a_dictionary)
+        return b_key
+    else:
+        return None
