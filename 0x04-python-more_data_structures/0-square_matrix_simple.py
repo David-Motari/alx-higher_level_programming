d7f356fbef7bbb210caafe1dab225696720db541
David Motari <davemotari22@gmail.com>
2022-06-08 12:17:26 +0000

    adding task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,16 @@
 # square_matrix computes the square value of
 #   all integers of a matrix.
 def square_matrix_simple(matrix=[]):
-    nw_matrix = [[(row[i] ** 2) for row in matrix] for i in range(len(matrix))]
-    return nw_matrix
+    # store len of matrix in rows variable.
+    # store len rows in cols variable.
+    # create new matrix new_matrix and return it.
+    new_matrix = []
+    rows = len(matrix)
+    cols = len(matrix[0])
+    for n in range(rows):
+        # create new row
+        n_row = []
+        for m in range(cols):
+            n_row.append((matrix[n][m]**2))
+        new_matrix.append(n_row)
+    return new_matrix
