02d981a9f7f833e71231ce9bed1414faccf968a5
dave motari <davemotari22@gmail.com>
2022-06-09 00:22:05 +0300

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,7 @@
 
 # Multiply values of dictionary by 2.
 def multiply_by_2(a_dictionary):
+    n_dictionary = {}
     for key in a_dictionary:
         n_dictionary.__setitem__(key, (a_dictionary[key] * 2))
     return n_dictionary
