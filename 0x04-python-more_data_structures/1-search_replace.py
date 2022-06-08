21ed1d9004c5d0cb1380f6e60a41dafe216c4919
dave motari <davemotari22@gmail.com>
2022-06-08 21:47:13 +0300

    Adding task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@
 # function that searches and replaces item in list and returns a new list.
 def search_replace(my_list, search, replace):
     i = my_list.index(search)
-    n_list = my_list[:i] + [replace] + [(i + 1):]
+    n_list = my_list[:i] + [replace] + my_list[(i + 1):]
     return n_list
