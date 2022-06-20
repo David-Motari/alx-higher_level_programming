32faf8872c75d16c89d16716ad09d1debabc3b85
dave motari <davemotari22@gmail.com>
2022-06-21 00:55:33 +0300

    Task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    n_arry = []
+    while i < list_length:
+        n = 0
+        try:
+            n = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+        except ZeroDivisionError:
+            print("division by 0")
+        except TypeError:
+            print("wrong type")
+        finally:
+            i += 1
+            n_arry.append(n)
+
+    return n_arry
