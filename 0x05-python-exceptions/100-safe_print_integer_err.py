e9f2dbab536ccedd5ff0c523b5a35f5a5c79ee07
dave motari <davemotari22@gmail.com>
2022-06-21 02:03:29 +0300

    Correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,9 @@ import sys
 
 
 def safe_print_integer_err(value):
+    """
+         function that prints an integer.
+    """
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
+    """
+        function that divides element by element 2 lists.
+    """
+    
     i = 0
     n_arry = []
     while i < list_length:
