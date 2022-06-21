75cda04abb02c1db8abad83c68c3195299c5702b
dave motari <davemotari22@gmail.com>
2022-06-21 23:55:31 +0300

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    1-square.py
+"""
+
+
+class Square:
+    """
+        defines a square
+    """
+    def __init__(self, size):
+        """
+            creates instance of square
+            args: size - size of a square.
+        """
+
+        self.__size = size
