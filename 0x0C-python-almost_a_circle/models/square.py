1b3c3c82774bf44d5f26010aeb65100d3d2b8d86
dave motari <davemotari22@gmail.com>
2022-07-12 23:11:30 +0300

    Task 10, 11

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+square.py
+inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    docstring
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        args: size - size of side,
+        x -  x-axis position
+        y - y-axis position
+        id - new square id
+        """
+        super().__init__(size,size,x,y,id)
+
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return f"[Square] ({self.id}) \
+{self.x}/{self.y} - {self.width}"
