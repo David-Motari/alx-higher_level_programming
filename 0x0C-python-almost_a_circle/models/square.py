05037a0659d42921fd2755f193f95214abc0fcc3
dave motari <davemotari22@gmail.com>
2022-07-12 23:22:22 +0300

    Task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,6 +32,46 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """
+        args: *args - array of arguments that reps new attribute values
+        - 1st argument represents id attribute
+        - 2nd argument represents sizeattribute
+        - 3rd argument represent x attribute
+        - 4th argument represents y attribute
+         **kwargs - 1st arg is id attribute 2nd arg can be\
+(size/x/y) attribute
+        """
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "id":
+                    if j is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = j
+                elif i == "size":
+                    self.size = j
+                elif i == "x":
+                    self.x = j
+                elif i == "y":
+                    self.y = j
+
     def __str__(self):
         return f"[Square] ({self.id}) \
 {self.x}/{self.y} - {self.width}"
