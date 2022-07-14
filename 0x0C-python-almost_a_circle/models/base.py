2af06a584c8380f1464888fe75fa6d01651c6ceb
dave motari <davemotari22@gmail.com>
2022-07-14 15:34:38 +0300

    Task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,7 +29,23 @@ class Base:
         returns: the string: "[]" if list_dictionaries is None
         else the JSON string representation of list_dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == "[]":
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
 
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        args: list_objs -  is a list of instances who inherits of Base
+
+        """
+        file_name = cls.__name__ + ".json"
+        with open(file_name, "w") as My_file:
+            if list_objs is None:
+                My_file.write("[]")
+
+            else:
+                for obj in list_objs:
+                    dict_list = obj.to_dictionary()
+                    My_file.write(Base.to_json_string(dict_list))
