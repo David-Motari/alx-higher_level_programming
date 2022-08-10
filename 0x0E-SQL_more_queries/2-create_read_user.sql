33b35543bb857ba53f0a54c209aa8c2d01041a22
dave motari <davemotari22@gmail.com>
2022-08-11 00:17:53 +0300

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creating user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 -- granting SELECT privileges
-GRANT SELECT ON hbtn_0d_2@ * TO user_0d_2@localhost;
-FLUSH PRIVILEGES
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
