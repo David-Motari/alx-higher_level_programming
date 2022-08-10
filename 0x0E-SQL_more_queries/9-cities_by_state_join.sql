0a3df2375a4d6e8379a402a44317c7a996674d1a
dave motari <davemotari22@gmail.com>
2022-08-11 01:23:17 +0300

    Task 7, 8, 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- lists all cities contained in the database hbtn_0d_usa.
-SELECT c.id c_id, c.name c_name, s.name s_name
-FROM cities c
-LEFT JOIN states s
-ON s.id = c.state_id 
-ORDER BY c_id;
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+LEFT JOIN states 
+ON states.id = cities.state_id 
+ORDER BY cities.id;
