7152734102d2414915ae4fbb11697c0198224373
dave motari <davemotari22@gmail.com>
2022-09-05 16:28:53 +0300

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv === 'undefined' && process.argv === 'null') {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+console.log(process.argv[2] || 'No argument');
