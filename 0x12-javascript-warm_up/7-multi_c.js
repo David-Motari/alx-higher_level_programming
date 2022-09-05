d569d24cb33e2e3b97a42da304549a488f097ff4
dave motari <davemotari22@gmail.com>
2022-09-05 22:27:09 +0300

    Task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ const x = parseInt(process.argv[2]);
 if (isNaN(x) === true) {
   console.log('Missing number of occurrences');
 } else {
-  let i = 0;
-  for (i; i < x; i++) {
-    console.log('C is fun ');
+  let i = 0; 
+  for (i; i < x; i++){
+    console.log('C is fun');
   }
 }
