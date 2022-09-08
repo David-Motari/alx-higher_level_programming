201e244a95c9d53a0b3fe9bfae9519d6c8c90075
dave motari <davemotari22@gmail.com>
2022-09-08 05:59:50 +0300

    C

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
-const data = require('./100-data').list;
-console.log(data);
-console.log(data.map((x, index) => x * index));
+const array1 = require('./100-data').list;
+console.log(array1);
+let i = 0;
+const map1 = array1.map(x => x * i++);
+
+console.log(map1);
