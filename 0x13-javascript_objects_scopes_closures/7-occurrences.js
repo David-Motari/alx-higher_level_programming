4978112940054dfd2752e8644d5ca30063e082ec
dave motari <davemotari22@gmail.com>
2022-09-08 08:52:21 +0300

    Task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+exports.nbOccurences = function (list, searchElement) {
+  let num = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) { num++; }
+  }
+  return num;
+};
