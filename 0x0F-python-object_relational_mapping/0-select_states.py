c407efc8e86715dc1795f96ba82750e7f66d6a13
David Motari <davemotari22@gmail.com>
2022-09-11 22:27:53 +0000

    Task 0 correction

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,14 +8,11 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        db_con = MySQLdb.connect(
-                user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-        curs = db_con.cursor()
-        curs.excute("SELECT * FROM `states`;")
-        states = curs.fetchall()
-        for state in states:
-            print(states)
-        db_con.close()
-    except FileNotFoundError:
-        print("Can't connect to database")
+    db_con = MySQLdb.connect(
+            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    curs = db_con.cursor()
+    curs.excute("SELECT * FROM `states`;")
+    states = curs.fetchall()
+    for state in states:
+        print(states)
+    db_con.close()
