b3dcf3817e7b8d2c0df81605c41fa0d50cdd3aca
David Motari <davemotari22@gmail.com>
2022-09-12 00:43:21 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     srch = argv[4]
-    cur.execute(
-            """SELECT * FROM states
-            WHERE name = {:s} ORDER BY id ASC""").format(srch)
+    cur.execute("""SELECT * FROM states WHERE name = '{:s}'
+    ORDER BY id ASC""").format(srch)
     states = cur.fetchall()
     for state in states:
         print(state)
