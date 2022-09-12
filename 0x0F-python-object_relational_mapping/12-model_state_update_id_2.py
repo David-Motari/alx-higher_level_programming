7f706d7460dd3e849a21dc2ec6ca37ecb8c181d9
David Motari <davemotari22@gmail.com>
2022-09-12 05:39:24 +0000

    Task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,7 +16,4 @@ if __name__ == "__main__":
     state = session.query(State).filter(State.id == 2).first()
     state.name = "New Mexico"
     session.commit()
-    states = session.query(State).order_by(State.id).all()
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
     session.close()
