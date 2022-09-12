a19311e135b7c68175ee9ba79ef0a78567bab404
David Motari <davemotari22@gmail.com>
2022-09-12 04:53:51 +0000

    Task 8 correction

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -17,4 +17,6 @@ if __name__ == "__main__":
     state = session.Query(State).order_by(State.id).first()
     if state:
         print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
     session.close()
