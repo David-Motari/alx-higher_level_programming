18cecf0287550dfbedc991eac73ba6aa4260a7c3
David Motari <davemotari22@gmail.com>
2022-09-12 00:54:54 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-import MySQLdb
 """
 takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument.
 """
+from sys import argv
+import MySQLdb
 
 
 if __name__ == "__main__":
