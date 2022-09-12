4b50f731492c54c0a69dfb801927c3dfdb9dc7aa
David Motari <davemotari22@gmail.com>
2022-09-12 00:00:54 +0000

    Task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+connecting to the db to make query
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    curs = db_con.cursor()
+    s = argv[4]
+    curs.execute("""SELECT * FROM `states`
+            WHERE name = s ORDER BY state.id ASC;""")
+    states = curs.fetchall()
+    for state in states:
+        print(state)
+    db_con.close()
