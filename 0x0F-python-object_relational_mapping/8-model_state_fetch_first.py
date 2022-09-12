cb24ad29d6cadd0fca27e0745bb748679e2ec1e6
David Motari <davemotari22@gmail.com>
2022-09-12 04:51:04 +0000

    Tasks correction

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,21 +2,19 @@
 """
 lists all State objects from the database hbtn_0e_6_usa
 """
-from model_state import Base, State
+from model_state import State, Base
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    state = session.query(State).order_by(State.id).first()
+    session = Session(bing=engine)
+    state = session.Query(State).order_by(State.id).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     session.close()
