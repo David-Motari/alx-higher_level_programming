6c40aec2513a60aef0ed2fd496125675f97d2136
David Motari <davemotari22@gmail.com>
2022-09-12 00:32:06 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
-connecting to the db to make query
-"""
-import MySQLdb
 from sys import argv
+import MySQLdb
+"""
+takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+"""
 
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states
-            WHERE name = {:s}
-            ORDER BY id ASC""").format(argv[4])
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    search = argv[4]
     states = cur.fetchall()
     for state in states:
-        print(state)
+        if state == search:
+            print(state)
     cur.close()
     conn.close()
