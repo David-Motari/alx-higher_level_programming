125efb2f5bbdd3a60cccacc082be4ef6ded2c7bb
David Motari <davemotari22@gmail.com>
2022-09-12 00:02:15 +0000

    Task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,7 +2,6 @@
 """
 connecting to the db to make query
 """
-
 import MySQLdb
 from sys import argv
 
@@ -11,7 +10,7 @@ if __name__ == "__main__":
     db_con = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     curs = db_con.cursor()
     s = argv[4]
-    curs.execute("""SELECT * FROM `states`
+    curs.execute("""SELECT * FROM states
             WHERE name = s ORDER BY state.id ASC;""")
     states = curs.fetchall()
     for state in states:
